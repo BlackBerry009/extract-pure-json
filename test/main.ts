1a71f54a603809153c7143617fc9e11e64b9cf6e
BlackBerry009 <765042894@qq.com>
2023-03-22 15:01:14 +0800

    feat: update case

--- test/main.ts
+++ test/main.ts
@@ -1,26 +1,32 @@
-import test from 'ava'
-import extractPureJson from '../src'
+import test from 'ava';
+import extractPureJson from '../src';
 
-test('extractPureJson', t => {
-    const text = 'hello world { "name": "test" }'
-    const result = extractPureJson(text)
-    t.deepEqual(result, { name: 'test' })
-})
+test('extractPureJson', (t) => {
+  const text = 'hello world { "name": "test" }';
+  const result = extractPureJson(text);
+  t.deepEqual(result, { name: 'test' });
+});
 
-test('extractPureJson with retry true', t => {
-    const text = 'hello world { "name": "test" }}'
-    const result = extractPureJson(text, { retry: 1 })
-    t.deepEqual(result, { name: 'test' })
-})
+test('extractPureJson with multiple json', (t) => {
+  const text = 'hello world { "name": "test" } { "name": "test" }';
+  const result = extractPureJson(text);
+  t.deepEqual(result, { name: 'test' });
+});
 
-test('extractPureJson with retry false', t => {
-    const text = 'hello world { "name": "test" }}'
-    const result = extractPureJson(text, { retry: 2 })
-    t.deepEqual(result, { name: 'test' })
-})
+test('extractPureJson with retry true', (t) => {
+  const text = 'hello world { "name": "test" }}';
+  const result = extractPureJson(text, { retry: 1 });
+  t.deepEqual(result, { name: 'test' });
+});
 
-test('extractPureJson with array', t => {
-    const text = '[{"a":1},{"b":2}]';
-    const result = extractPureJson(text);
-    t.deepEqual(result, false);
-})
+test('extractPureJson with retry false', (t) => {
+  const text = 'hello world { "name": "test" }}';
+  const result = extractPureJson(text, { retry: 2 });
+  t.deepEqual(result, { name: 'test' });
+});
+
+test('extractPureJson with array', (t) => {
+  const text = '[{"a":1},{"b":2}]';
+  const result = extractPureJson(text);
+  t.deepEqual(result, false);
+});
